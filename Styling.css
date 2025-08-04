f7c30b31c294de757f1340092def19597e8cef46
muhajer456coding <alexisbriden3@gmail.com>
2025-08-04 19:29:01 +0430

    Create Styling.css

---
+++ Styling.css
@@ -0,0 +1,23 @@
+function calculate() {
+  const java = Number(document.getElementById("java").value);
+  const comp = Number(document.getElementById("comp").value);
+  const math = Number(document.getElementById("math").value);
+  const sirat = Number(document.getElementById("sirat").value);
+  const nezam = Number(document.getElementById("nezam").value);
+  const english = Number(document.getElementById("english").value);
+
+  const totalWeighted = 
+      (java * 5) +
+      (comp * 5) +
+      (math * 5) +
+      (sirat * 2) +
+      (nezam * 2) +
+      (english * 2);
+
+  const totalCredits = 5 + 5 + 5 + 2 + 2 + 2; // 21
+
+  const percentage = totalWeighted / totalCredits;
+
+  document.getElementById("result").innerText = 
+      " The whole percentage you got: " + percentage.toFixed(2);
+}
